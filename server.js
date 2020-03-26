2c12af5de481d1246d32359f1f6506fc4303eefe
Joris <guienjoris@gmail.com>
2020-03-26 13:43:30 +0100

    update deploy

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
 
 // Set public folder as root
 app.use(express.static('public'));
@@ -12,6 +11,6 @@ app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-app.listen(port, () => {
-  console.info('listening on %d', port);
+app.listen(process.env.PORT , () => {
+  console.info('listening on %d',process.env.PORT );
 });
